75d28a157228a8c64fe484d935649392f4fa9ad8
BbrianWright <bbrian.wright@gmail.com>
2012-11-30 20:49:25 -0500

    add story teststaaa

---
+++ Adventuregram/src/com/adventureteamllc/adventuregram/StoryTest.java
@@ -0,0 +1,34 @@
+package com.adventureteamllc.adventuregram;
+//import static org.junit.Assert.*;
+import junit.framework.TestCase;
+//import org.junit.Test;
+
+
+public class StoryTest
+extends TestCase
+{
+
+private Story story;
+private Event event;
+
+/**
+ * Initialize test fixtures
+ */
+public void setUp()
+{
+   story = new Story();
+   event = new Event("title", "descrip");
+}
+
+/**
+ * Tests story methods
+ */
+public void testStory()
+{
+   story.addEvent(event);
+   assertEquals(event, story.getEvent(0));
+   assertEquals(1, story.getNumEvents());
+}
+
+
+}
