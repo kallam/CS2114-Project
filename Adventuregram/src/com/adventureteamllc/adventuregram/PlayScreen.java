ea5b27c9126059208a8b00ebce5f8c49762fa680
Brian Wright <Brian Wright@G50Laptop>
2012-12-07 17:08:17 -0500

    play additions

--- Adventuregram/src/com/adventureteamllc/adventuregram/PlayScreen.java
+++ Adventuregram/src/com/adventureteamllc/adventuregram/PlayScreen.java
@@ -18,6 +18,47 @@ public class PlayScreen
     private Button choice3;
     private Button choice4;
 
+    //opens are you sure? progress won't be saved?
+    private Button exitLibrary;
+
     private TextView storyTitle;
     private TextView eventDescription;
+
+    public void Initialize()
+    {
+        //set storyTitle text and eventDescrip from text file
+        //initialize buttons and their text
+        //if buttons have no reference then hide or disable them
+    }
+
+    //must advance to next event
+    //check if event is the last
+    /**
+     * Advance story with this choice
+     * choice1 should always be an option
+     */
+    public void choice1Clicked()
+    {
+
+    }
+    public void choice2Clicked()
+    {
+
+    }
+    public void choice3Clicked()
+    {
+
+    }
+    public void choice4Clicked()
+    {
+
+    }
+    /**
+     * Open window to prompt exit, then exit if yes
+     */
+    public void exitLibraryClicked()
+    {
+
+    }
+
 }
