eaea4a348fa24de9fa2e3f5c3db3fa7ab49a0a6b
Uncle Jemima 77 <supertones333@hotmail.com>
2012-12-07 21:37:52 -0500

    Da Graphics

--- Adventuregram/src/com/adventureteamllc/adventuregram/MainTitleScreen.java
+++ Adventuregram/src/com/adventureteamllc/adventuregram/MainTitleScreen.java
@@ -1,5 +1,8 @@
 package com.adventureteamllc.adventuregram;
 
+import android.graphics.RectF;
+import sofia.graphics.ImageShape;
+import sofia.app.ShapeScreen;
 import sofia.app.Screen;
 
 // -------------------------------------------------------------------------
@@ -10,14 +13,26 @@ import sofia.app.Screen;
  *  @author ADVENTURETEAMLLC
  *  @version Nov 6, 2012
  */
-public class MainTitleScreen extends Screen {
+public class MainTitleScreen extends ShapeScreen {
 
-   /**
+    private ImageShape image;
+
+    /**
+     * Shows the opening image and credits as an image.
+     */
+    public void initialize()
+    {
+        image = new ImageShape("bad_drawing", new RectF(0, 0, getWidth(),
+            getHeight()));
+        add(image);
+    }
+
+    /**
      * Present the LibraryScreen to the user then end the MainTitleScreen.
      */
     public void button1Clicked()
     {
-        presentScreen(LibraryScreen.class);
+//        presentScreen(LibraryScreen.class);
         finish();
     }
 
